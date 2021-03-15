02c6951ec7e946bd3abdd44c70c5b261683575fb
DefCon-018 <hp761570@gmil.com>
2021-03-15 16:49:41 +0530

    Add action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
-{
-    type: 'ADD_MOVIES';
-    movies: [];
+
+// action types(variable) 
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies(movies){
+    return {
+        type: ADD_MOVIES,
+        movies
+    }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import { data } from '../data';
+import { addMovies } from '../actions';
 
 class App extends React.Component {
 
@@ -12,10 +13,7 @@ class App extends React.Component {
       console.log("Updated");
       this.forceUpdate();
     })
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    })
+    store.dispatch(addMovies(data));
     console.log('store after ', store.getState());
   }
 
