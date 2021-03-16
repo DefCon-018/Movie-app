366ace8852c381dfc54c185c3b2c332cb5a9e005
DefCon-018 <hp761570@gmil.com>
2021-03-16 12:18:54 +0530

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// Middleware
+const logger = function({dispatch, getState}){
+  return function(next){
+    return function(action){
+      console.log(action.type);
+      next(action);
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 // console.log('state', store.getState());
 // store.dispatch({
 //   type: 'ADD_MOVIES',
